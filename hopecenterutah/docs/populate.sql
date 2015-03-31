74e4083634386b7c415ef71089b7b7c8f767e3c6
Bones687 <neildalton6@gmail.com>
2015-03-31 11:26:25 -0600

    populate user table script

---
+++ hopecenterutah/docs/populate.sql
@@ -0,0 +1,8 @@
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Neil', 'Dalton', 'neildalton@weber.edu', 'SU', 'password');
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Tanner', 'Burton', 'tannerburton@mail.weber.edu', 'SU', 'password');
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Lincoln', 'Jarman', 'lincolnjarman@gmail.com', 'SU', 'password');
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Brody', 'Shiba', 'brodyshiba@gmail.com', 'SU', 'password');
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Jared', 'Smith', 'jaresmith22@gmail.com', 'SU', 'password');
+INSERT INTO `HOPECENTERUTAH`.`User` (`user_first_name`, `user_last_name`, `user_email`, `user_type`, `user_pass`) VALUES ('Adam', 'Gessel', 'damageless@gmail.com', 'SU', 'password');
+
+
